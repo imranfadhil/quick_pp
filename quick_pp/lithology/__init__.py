861cf578062b00c11df614297ced6f20b59ba9fd
M Imran B M Fadhil <imran.fadhil@petronas.com>
2024-01-19 15:19:45 +0800

    clean up __init__ in lithology module

--- quick_pp/lithology/__init__.py
+++ quick_pp/lithology/__init__.py
@@ -1,24 +1,4 @@
-from .sand_silt_clay import sand_silt_clay_model
-from .multi_mineral import multi_mineral_model
-from .sand_shale import sand_shale_model
-from .shale import *
-
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-
-
-def normalize_lithology(df: pd.DataFrame, input_features: list):
-    """Normalize lithology curves for better visualization
-
-    Args:
-        df (pd.DataFrame): _description_
-
-    Returns:
-        pd.DataFrame: _description_
-    """
-    scaler = MinMaxScaler()
-    transposed = df[input_features].T
-    return_df = pd.DataFrame(scaler.fit_transform(transposed).T, columns=input_features)
-    df[input_features] = return_df[input_features]
-
-    return df
+from . sand_silt_clay import sand_silt_clay_model
+from . multi_mineral import multi_mineral_model
+from . sand_shale import sand_shale_model
+from . shale import *
