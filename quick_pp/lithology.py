384c4347f86cebaaf7ef5a1de627cf805ca99cd0
M Imran B M Fadhil <imran.fadhil@petronas.com>
2024-01-20 14:50:53 +0800

    refactor lithology module

--- quick_pp/lithology.py
+++ quick_pp/lithology.py
@@ -14,15 +14,17 @@ from .config import Config
 class SandSiltClay:
     """Sand-silt-clay model"""
 
-    def sand_silt_clay_model(self, nphi, rhob, model: str = 'kuttan',
-                             dry_sand_point: tuple = None,
-                             dry_silt_point: tuple = None,
-                             dry_clay_point: tuple = None,
-                             fluid_point: tuple = None,
-                             wet_clay_point: tuple = None,
-                             silt_line_angle: float = None,
-                             xplot: bool = False,
-                             normalize: bool = True):
+    def __init__(self, dry_sand_point: tuple = None, dry_silt_point: tuple = None, dry_clay_point: tuple = None,
+                 fluid_point: tuple = None, wet_clay_point: tuple = None, silt_line_angle: float = None):
+        # Initialize the endpoints
+        self.dry_sand_point = dry_sand_point or Config.SSC_ENDPOINTS["DRY_SAND_POINT"]
+        self.dry_silt_point = dry_silt_point or Config.SSC_ENDPOINTS["DRY_SILT_POINT"]
+        self.dry_clay_point = dry_clay_point or Config.SSC_ENDPOINTS["DRY_CLAY_POINT"]
+        self.fluid_point = fluid_point or Config.SSC_ENDPOINTS["FLUID_POINT"]
+        self.wet_clay_point = wet_clay_point or Config.SSC_ENDPOINTS["WET_CLAY_POINT"]
+        self.silt_line_angle = silt_line_angle or Config.SSC_ENDPOINTS["SILT_LINE_ANGLE"]
+
+    def sand_silt_clay_model(self, nphi, rhob, model: str = 'kuttan', xplot: bool = False, normalize: bool = True):
         """Estimate lithology volumetrics based on Kuttan's and simplification of PCSB's litho-porosity model
 
         Args:
@@ -42,56 +44,49 @@ class SandSiltClay:
         """
         assert model in ['kuttan', 'kuttan_modified'], f"'{model}' model is not available."
         # Initialize the endpoints
-        A = dry_sand_point or Config.SSC_ENDPOINTS["DRY_SAND_POINT"]
-        B = dry_silt_point or Config.SSC_ENDPOINTS["DRY_SILT_POINT"]
-        C = dry_clay_point or Config.SSC_ENDPOINTS["DRY_CLAY_POINT"]
-        D = fluid_point or Config.SSC_ENDPOINTS["FLUID_POINT"]
-        wetclay_pt = wet_clay_point or Config.SSC_ENDPOINTS["WET_CLAY_POINT"]
-        silt_line_angle = silt_line_angle or Config.SSC_ENDPOINTS["SILT_LINE_ANGLE"]
+        A = self.dry_sand_point
+        B = self.dry_silt_point
+        C = self.dry_clay_point
+        D = self.fluid_point
 
         # Redefine wetclay point
         _, rhob_max_line = min_max_line(rhob, 0.05)
         _, nphi_max_line = min_max_line(nphi, 0.05)
         wetclay_RHOB = np.min(rhob_max_line)
         wetclay_NPHI = np.max(nphi_max_line)
-        if not all(wetclay_pt):
-            wetclay_pt = (wetclay_NPHI, wetclay_RHOB)
-        # print(f'#### wetclay_pt: {wetclay_pt}')
+        if not all(self.wet_clay_point):
+            self.wet_clay_point = (wetclay_NPHI, wetclay_RHOB)
+        # print(f'#### wet_clay_point: {wet_clay_point}')
 
         # Redefine drysilt point
-        drysilt_NPHI = 1 - 1.68*(math.tan(float(silt_line_angle - 90)*math.pi / 180))
+        drysilt_NPHI = 1 - 1.68*(math.tan(float(self.silt_line_angle - 90)*math.pi / 180))
         if not all(B):
-            B = (drysilt_NPHI, B[1])
+            B = self.dry_silt_point = (drysilt_NPHI, B[1])
         # print(f'#### drysilt_pt: {drysilt_pt}')
 
         # Define dryclay point
         if not all(C):
-            m = (D[1] - wetclay_pt[1]) / (D[0] - wetclay_pt[0])
+            m = (D[1] - self.wet_clay_point[1]) / (D[0] - self.wet_clay_point[0])
             dryclay_NPHI = ((C[1] - D[1]) / m) + D[0]
-            C = (dryclay_NPHI, C[1])
+            C = self.dry_clay_point = (dryclay_NPHI, C[1])
         # print(f'#### dryclay_pt: {C}, {m}')
 
         if model == 'kuttan':
-            vsand, vsilt, vcld = self.lithology_fraction_kuttan(nphi, rhob, A, B, C, D, normalize)
+            vsand, vsilt, vcld = self.lithology_fraction_kuttan(nphi, rhob, normalize)
         else:
-            vsand, vsilt, vcld = self.lithology_fraction_kuttan_modified(nphi, rhob, A, B, C, D, normalize)
+            vsand, vsilt, vcld = self.lithology_fraction_kuttan_modified(nphi, rhob, normalize)
 
         # Calculate vclb: volume of clay bound water
         clay_phit = clay_porosity(rhob_max_line, C[1])
         vclb = vcld * clay_phit
 
         if xplot:
-            fig = neutron_density_xplot(nphi, rhob, A, B, C, D, wetclay_pt)
+            fig = neutron_density_xplot(nphi, rhob, A, B, C, D, self.wet_clay_point)
             return vsand, vsilt, vcld, vclb, fig
         else:
             return vsand, vsilt, vcld, vclb, None
 
-    def lithology_fraction_kuttan(self, nphi, rhob,
-                                  dry_sand_point: tuple = None,
-                                  dry_silt_point: tuple = None,
-                                  dry_clay_point: tuple = None,
-                                  fluid_point: tuple = None,
-                                  normalize: bool = True):
+    def lithology_fraction_kuttan(self, nphi, rhob, normalize: bool = True):
         """_summary_
 
         Args:
@@ -105,10 +100,10 @@ class SandSiltClay:
         Returns:
             _type_: _description_
         """
-        A = dry_sand_point
-        B = dry_silt_point
-        C = dry_clay_point
-        D = fluid_point
+        A = self.dry_sand_point
+        B = self.dry_silt_point
+        C = self.dry_clay_point
+        D = self.fluid_point
         E = list(zip(nphi, rhob))
         rocklithofrac = length_a_b(A, C)
         sandsiltfrac = length_a_b(A, B)
@@ -136,12 +131,7 @@ class SandSiltClay:
 
         return vsand, vsilt, vcld
 
-    def lithology_fraction_kuttan_modified(self, nphi, rhob,
-                                           dry_sand_point: tuple = None,
-                                           dry_silt_point: tuple = None,
-                                           dry_clay_point: tuple = None,
-                                           fluid_point: tuple = None,
-                                           normalize: bool = True):
+    def lithology_fraction_kuttan_modified(self, nphi, rhob, normalize: bool = True):
         """_summary_
 
         Args:
@@ -155,10 +145,10 @@ class SandSiltClay:
         Returns:
             _type_: _description_
         """
-        A = dry_sand_point
-        B = dry_silt_point
-        C = dry_clay_point
-        D = fluid_point
+        A = self.dry_sand_point
+        B = self.dry_silt_point
+        C = self.dry_clay_point
+        D = self.fluid_point
         E = list(zip(nphi, rhob))
 
         siltclayratio = 0.4  # empirical value
@@ -325,13 +315,17 @@ class MultiMineral():
 class SandShale:
     """Sand-shale model"""
 
-    def sand_shale_model(self, nphi, rhob,
-                         dry_sand_point: tuple = None,
-                         dry_clay_point: tuple = None,
-                         fluid_point: tuple = None,
-                         wet_clay_point: tuple = None,
-                         silt_line_angle: float = None,
-                         xplot: bool = False):
+    def __init__(self, dry_sand_point: tuple = None, dry_silt_point: tuple = None, dry_clay_point: tuple = None,
+                 fluid_point: tuple = None, wet_clay_point: tuple = None, silt_line_angle: float = None):
+        # Initialize the endpoints
+        self.dry_sand_point = dry_sand_point or Config.SSC_ENDPOINTS["DRY_SAND_POINT"]
+        self.dry_silt_point = dry_silt_point or Config.SSC_ENDPOINTS["DRY_SILT_POINT"]
+        self.dry_clay_point = dry_clay_point or Config.SSC_ENDPOINTS["DRY_CLAY_POINT"]
+        self.fluid_point = fluid_point or Config.SSC_ENDPOINTS["FLUID_POINT"]
+        self.wet_clay_point = wet_clay_point or Config.SSC_ENDPOINTS["WET_CLAY_POINT"]
+        self.silt_line_angle = silt_line_angle or Config.SSC_ENDPOINTS["SILT_LINE_ANGLE"]
+
+    def sand_shale_model(self, nphi, rhob, xplot: bool = False):
         """Estimate lithology volumetrics based on Kuttan's litho-porosity model
 
         Args:
@@ -344,43 +338,38 @@ class SandShale:
             _type_: _description_
         """
         # Initialize the endpoints
-        A = dry_sand_point or Config.SSC_ENDPOINTS["DRY_SAND_POINT"]
-        C = dry_clay_point or Config.SSC_ENDPOINTS["DRY_CLAY_POINT"]
-        D = fluid_point or Config.SSC_ENDPOINTS["FLUID_POINT"]
-        wetclay_pt = wet_clay_point or Config.SSC_ENDPOINTS["WET_CLAY_POINT"]
-        silt_line_angle = silt_line_angle or Config.SSC_ENDPOINTS["SILT_LINE_ANGLE"]
+        A = self.dry_sand_point
+        C = self.dry_clay_point
+        D = self.fluid_point
 
         # Redefine wetclay point
         _, rhob_max_line = min_max_line(rhob, 0.05)
         _, nphi_max_line = min_max_line(nphi, 0.05)
         wetclay_RHOB = np.min(rhob_max_line)
         wetclay_NPHI = np.max(nphi_max_line)
-        if not all(wetclay_pt):
-            wetclay_pt = (wetclay_NPHI, wetclay_RHOB)
-        # print(f'#### wetclay_pt: {wetclay_pt}')
+        if not all(self.wet_clay_point):
+            self.wet_clay_point = (wetclay_NPHI, wetclay_RHOB)
+        # print(f'#### wet_clay_point: {wet_clay_point}')
 
         # Define dryclay point
         if not all(C):
-            m = (D[1] - wetclay_pt[1]) / (D[0] - wetclay_pt[0])
+            m = (D[1] - self.wet_clay_point[1]) / (D[0] - self.wet_clay_point[0])
             dryclay_NPHI = ((C[1] - D[1]) / m) + D[0]
-            C = (dryclay_NPHI, C[1])
+            C = self.dry_clay_point = (dryclay_NPHI, C[1])
         # print(f'#### dryclay_pt: {C}, {m}')
 
-        vsand, vcld = self.lithology_fraction(nphi, rhob, A, C, D)
+        vsand, vcld = self.lithology_fraction(nphi, rhob)
 
         if xplot:
-            fig = neutron_density_xplot(nphi, rhob, A, (0, 0), C, D, wetclay_pt)
+            fig = neutron_density_xplot(nphi, rhob, A, (0, 0), C, D, self.wet_clay_point)
             return vsand, vcld, fig
         else:
             return vsand, vcld, None
 
-    def lithology_fraction(self, nphi, rhob,
-                           dry_sand_point: tuple = None,
-                           dry_clay_point: tuple = None,
-                           fluid_point: tuple = None):
-        A = dry_sand_point
-        C = dry_clay_point
-        D = fluid_point
+    def lithology_fraction(self, nphi, rhob):
+        A = self.dry_sand_point
+        C = self.dry_clay_point
+        D = self.fluid_point
         E = list(zip(nphi, rhob))
         rocklithofrac = length_a_b(A, C)
 
