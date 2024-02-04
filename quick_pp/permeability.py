81da7fe85d6a92859b78494c632888b64019c901
M Imran B M Fadhil <imran.fadhil@petronas.com>
2024-02-04 15:58:02 +0800

    Update docstrings in permeability

--- quick_pp/permeability.py
+++ quick_pp/permeability.py
@@ -1,30 +1,30 @@
 def choo_permeability(vclw, vsilt, phit, m=2, B=None, A=20e7, C=2):
-    """_summary_
+    """Estimate permeability using Choo's equation.
 
     Args:
-        vclw (_type_): _description_
-        vsilt (_type_): _description_
-        phit (_type_): _description_
-        A (_type_): 0.125 * rg**2 / 10
-        B (_type_): Constant or m * ((2 / C) + 1) + 2
-        C (_type_): 2
+        vclw (float): Volume of clay in fraction.
+        vsilt (float): Volume of silt in fraction.
+        phit (float): Total porosity in fraction.
+        A (float): Constant based on 0.125 * rg**2 / 10. Defaults to 20e7.
+        B (float): Constant, defaults m * ((2 / C) + 1) + 2.
+        C (float): Constant, defaults to 2.
 
     Returns:
-        _type_: _description_
+        float: Choo's permeability in mD.
     """
     B = B or m * ((2 / C) + 1) + 2
     return A * phit**B / 10**(6 * vclw + 3 * vsilt)
 
 
 def kozeny_carman_permeability(phit, S=0.01):
-    """_summary_
+    """Estimate permeability using Kozeny-Carman's equation.
 
     Args:
-        phit (v/v): _description_
-        S (_type_): Specific surface area of the grains
+        phit (float): Total porosity in fraction.
+        S (float): Specific surface area of the grains
 
     Returns:
-        _type_: _description_
+        float: Permeability in mD.
     """
     return phit**3 / (5 * S**2 * (1 - phit**2))
 
@@ -33,15 +33,15 @@ def permeability_thickness(phit, Swirr, A, B, C):
     """KH = A * phit**B / Swirr**C
 
     Args:
-        phit (_type_): _description_
-        Swirr (_type_): _description_
-        h (_type_): _description_
-        A (_type_): _description_
-        B (_type_): _description_
-        C (_type_): _description_
+        phit (float): _description_
+        Swirr (float): _description_
+        h (float): _description_
+        A (float): _description_
+        B (float): _description_
+        C (float): _description_
 
     Returns:
-        _type_: _description_
+        float: _description_
     """
     return A * phit**B / Swirr**C
 
@@ -51,11 +51,11 @@ def timur_permeability(phit, Swirr):
     oil fields in North America.
 
     Args:
-        phit (%): Porosity
-        Swirr (%): Irreduceable water saturation
+        phit (float): Porosity in fraction.
+        Swirr (float): Irreduceable water saturation in fraction.
 
     Returns:
-        _type_: Permeability in mD
+        float: Permeability in mD.
     """
     return 8.58 * phit**4.4 / Swirr**2
 
@@ -64,12 +64,12 @@ def coates_permeability(phie, Swirr, a=1):
     """Based on Coates (1974)
 
     Args:
-        phie (%): Effective porosity
-        Swirr (%): Irreduceable water saturation
-        a (float): Constant
+        phie (float): Effective porosity in fraction.
+        Swirr (float): Irreduceable water saturation in fraction.
+        a (float): Constant. Defaults to 1.
 
     Returns:
-        _type_: _description_
+        float: Coates permeability in mD.
     """
     return (phie**2 / a * (1 - Swirr) / Swirr)**2
 
@@ -78,11 +78,11 @@ def tixier_permeability(phit, Swirr):
     """Based on Tixier (1949)
 
     Args:
-        phit (%): Porosity
-        Swirr (%): Irreduceable water saturation
+        phit (float): Porosity in fraction.
+        Swirr (float): Irreduceable water saturation in fraction.
 
     Returns:
-        _type_: Permeability in mD
+        float: Permeability in mD
     """
     return 62.5 * phit**6 / Swirr**2
 
@@ -91,11 +91,11 @@ def morris_biggs_permeability(phit, phie, Vbwi):
     """Based on Morris and Biggs (1967)
 
     Args:
-        phit (%): Total porosity
-        phie (%): Effective porosity
-        Vbwi (%): Volume of bound water
+        phit (float): Total porosity in fraction.
+        phie (float): Effective porosity in fraction.
+        Vbwi (float): Volume of bound water in fraction.
 
     Returns:
-        _type_: _description_
+        float: Permeability in mD.
     """
     return (1e2 * phie**2 * (phit - Vbwi) / Vbwi)**2
