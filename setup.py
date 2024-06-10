7aa14852cbb2e5f1b81bfe20a18ad586557b9eda
M Imran B M Fadhil <imran.fadhil@petronas.com>
2024-06-10 17:30:15 +0800

    update requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # Steps:
 # 1. "python -m build" to create new version of source archive and wheel in dist/
-# 2. "python -m twine upload dist/*" to upload to PyPI
+# 2. "python -m twine upload dist/*" to upload to PyPI or "twine upload -r testpypi dist/*" to upload to TestPyPI
 # 2.a. Username: __token__
 # 2.b. Password: <token>
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import setup  # find_packages
 import quick_pp
 
 with open('README.rst') as readme_file:
@@ -16,7 +16,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'ruptures', 'welly', 'pandas', 'numpy']
 
 test_requirements = []
 
@@ -32,18 +32,18 @@ setup(
         'Programming Language :: Python :: 3.10',
     ],
     description="Python package to assist in providing quick-look/ preliminary petrophysical estimation.",
-    entry_points={
-        'console_scripts': [
-            'quick_pp=quick_pp.cli:main',
-        ],
-    },
+    # entry_points={
+    #     'console_scripts': [
+    #         'quick_pp=quick_pp.cli:main',
+    #     ],
+    # },
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='quick_pp',
+    keywords='quick_pp, petrophysics, geoscience, well log analysis',
     name='quick_pp',
-    packages=find_packages(include=['quick_pp', 'quick_pp.*']),
+    packages=('quick_pp', ),  # find_packages(include=['quick_pp', 'quick_pp.*']),  # where='quick_pp'),  #
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/imranfadhil/quick_pp',
