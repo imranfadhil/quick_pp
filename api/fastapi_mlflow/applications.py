a27d54c59cd98b52b931ef17314d2b49b98dd44c
imranfadhil <imranfadhil@gmail.com>
2025-05-18 23:46:13 +0800

    add fastapi-mlflow module and add model deployment

---
+++ api/fastapi_mlflow/applications.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Build FastAPI applications for mlflow model predictions.
+
+Copyright (C) 2022, Auto Trader UK
+
+"""
+
+import logging
+from inspect import signature
+
+from fastapi import FastAPI, Request
+from fastapi.responses import ORJSONResponse
+from mlflow.pyfunc import PyFuncModel  # type: ignore
+
+from fastapi_mlflow.exceptions import DictSerialisableException
+from fastapi_mlflow.predictors import build_predictor
+
+
+def build_app(app, pyfunc_model: PyFuncModel, route: str) -> FastAPI:
+    """Build and return a FastAPI app for the mlflow model."""
+    logger = logging.getLogger(__name__)
+    predictor = build_predictor(pyfunc_model)
+    response_model = signature(predictor).return_annotation
+    app.add_api_route(
+        f"{route}/predictions",
+        predictor,
+        response_model=response_model,
+        response_class=ORJSONResponse,
+        methods=["POST"],
+    )
+
+    @app.exception_handler(DictSerialisableException)
+    def handle_serialisable_exception(
+        req: Request, exc: DictSerialisableException
+    ) -> ORJSONResponse:
+        nonlocal logger
+        req_id = req.headers.get("x-request-id")
+        extra = {"x-request-id": req_id} if req_id is not None else {}
+        logger.exception(exc.message, extra=extra)
+        return ORJSONResponse(
+            status_code=500,
+            content=exc.to_dict(),
+        )
+
+    @app.exception_handler(Exception)
+    def handle_exception(req: Request, exc: Exception) -> ORJSONResponse:
+        return handle_serialisable_exception(
+            req, DictSerialisableException.from_exception(exc)
+        )
+
+    return app
---
+++ api/mlflow_model_deployment.py
@@ -0,0 +1,90 @@
+from fastapi import FastAPI, Request
+from starlette.types import Message
+from datetime import datetime
+import mlflow
+from mlflow.pyfunc import load_model
+
+# # Uncomment below 3 lines to run >> if __name__ == "__main__"
+# import os
+# import sys
+# sys.path.append(os.getcwd())
+
+from api.fastapi_mlflow.applications import build_app
+
+
+def get_model_info(registered_model):
+    """Extract information of the registered models to load models automatically for predictions.
+
+    Args:
+        registered_model (class): Model that has been loaded and registered
+    """
+    model_info = {}
+
+    for model in registered_model:
+        model_info['reg_model_name'] = model.name
+        model_info['run_id'] = model.run_id
+        model_info['version'] = model.version
+        model_info['model_uri'] = model.source
+        model_info['stage'] = model.current_stage
+    return model_info
+
+
+app = FastAPI(title='ERMAI RTP - ML Models', debug=True)
+
+
+# set_body and get_body required to extract request body in middleware
+async def set_body(request: Request, body: bytes):
+    async def receive() -> Message:
+        return {"type": "http.request", "body": body}
+    request._receive = receive
+
+
+async def get_body(request: Request) -> bytes:
+    body = await request.body()
+    await set_body(request, body)
+    return body
+
+
+client = mlflow.tracking.MlflowClient()
+
+start_time = datetime.now()
+model_count = 0
+print(f"Tracking uri {mlflow.get_tracking_uri()}")
+
+try:
+    # Get latest registered models
+    for rm in client.search_registered_models():
+        model_info = get_model_info(rm.latest_versions)
+        if 'reg_model_name' in model_info.keys():
+            model_uri = f"models:/{model_info['reg_model_name']}/{model_info['version']}"
+            model = load_model(model_uri)
+
+            model_count += 1
+            # Build API for the loaded model
+            route = fr"/{model_info['reg_model_name']}"
+            app = build_app(app, model, route)
+            print(f"Mounting #{model_count} | {route}")
+
+except Exception as e:
+    print(f"Mounting #{model_count} | {model_info['reg_model_name']}/{model_info['version']} - Error: {e}")
+
+duration = round((datetime.now() - start_time).total_seconds() / 60, 3)
+print(f"Completed mounting {model_count} models in {duration} minutes")
+
+
+def build_model_api():
+    return app
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    from quick_pp.modelling.config import MLFLOW_CONFIG
+
+    # Make sure mlflow server is running first
+    env = 'local'
+    mlflow.set_tracking_uri(
+        f"http://{MLFLOW_CONFIG[env]['tracking_host']}:{MLFLOW_CONFIG[env]['tracking_port']}")
+    print(f"MLflow tracking URI: {mlflow.get_tracking_uri()}")
+
+    uvicorn.run("api.mlflow_model_deployment:app", host='0.0.0.0', port=5555, reload=False)
