eadb9796fb4f96861e22c83f79e6b19740067864
imranfadhil <imranfadhil@gmail.com>
2025-10-23 11:00:10 +0800

    update version 0.2.67

--- quick_pp/__init__.py
+++ quick_pp/__init__.py
@@ -20,4 +20,4 @@ logging.getLogger("mlflow").setLevel(logging.WARNING)
 
 __author__ = """Imran Fadhil"""
 __email__ = 'imranfadhil@gmail.com'
-__version__ = '0.2.66'  # Need to be updated manually when releasing a new version/ change in pyproject.toml
+__version__ = '0.2.67'  # Need to be updated manually when releasing a new version/ change in pyproject.toml
